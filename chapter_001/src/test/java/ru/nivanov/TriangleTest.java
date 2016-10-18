e856d693b4752079f9872088c26088925e23177b
Piterski72 <piterski@yandex.ru>
2016-10-18 20:54:21 +0300

    corr Trian Test

--- chapter_001/src/test/java/ru/nivanov/TriangleTest.java
+++ chapter_001/src/test/java/ru/nivanov/TriangleTest.java
@@ -9,14 +9,14 @@ public class TriangleTest {
 	@Test
 	public void whenABCCorrectThenReturnArea() {
 	  Triangle abcOne=new Triangle(new Point(2.0,1.0), new Point (-1.0,-3.0), new Point(-1.0,1.0));
-      double ploshad=ABC_One.area ();
+      double ploshad=abcOne.area ();
       assertThat(ploshad, is (6.0d));
     }
 	
     @Test
 	public void whenABCinLineThenReturnError() {
 	  Triangle abcTwo=new Triangle(new Point(1.0,1.0), new Point (5.0,5.0), new Point(10.0,10.0));
-	  double ploshad=ABC_Two.area ();
+	  double ploshad=abcTwo.area ();
       assertThat(ploshad, is (0.0d));	  
 	}
 	
