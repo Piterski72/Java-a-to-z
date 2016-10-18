87cff8c0c3d68ca64c5393a950ce5f3bd6c65eb6
Piterski72 <piterski@yandex.ru>
2016-10-18 17:04:50 +0300

    correct TriangleTest

--- chapter_001/src/test/java/ru/nivanov/TriangleTest.java
+++ chapter_001/src/test/java/ru/nivanov/TriangleTest.java
@@ -8,14 +8,14 @@ public class TriangleTest {
 	
 	@Test
 	public void whenABCCorrectThenReturnArea() {
-	  Triangle ABC_One=new Triangle(new Point(2.0,1.0), new Point (-1.0,-3.0), new Point(-1.0,1.0));
+	  Triangle abcOne=new Triangle(new Point(2.0,1.0), new Point (-1.0,-3.0), new Point(-1.0,1.0));
       double ploshad=ABC_One.area ();
       assertThat(ploshad, is (6.0d));
     }
 	
     @Test
 	public void whenABCinLineThenReturnError() {
-	  Triangle ABC_Two=new Triangle(new Point(1.0,1.0), new Point (5.0,5.0), new Point(10.0,10.0));
+	  Triangle abcTwo=new Triangle(new Point(1.0,1.0), new Point (5.0,5.0), new Point(10.0,10.0));
 	  double ploshad=ABC_Two.area ();
       assertThat(ploshad, is (0.0d));	  
 	}
