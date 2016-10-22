ad02e1406b67c10acc43dd18bdffb4bb84df3b9c
Piterski72 <piterski@yandex.ru>
2016-10-22 11:06:15 +0300

    add comments in BubbleSortMass

--- chapter_001/src/main/java/ru/nivanov/BubbleSortMass.java
+++ chapter_001/src/main/java/ru/nivanov/BubbleSortMass.java
@@ -10,9 +10,13 @@ public class BubbleSortMass {
 	
 	public void bubbleSort(int[] values){
     
+		// Внешний цикл за проход сокращает фрагмент массива, внутренний перегоняет в конец макс. элемент.
+		
 		for(int i = values.length-1 ; i > 0 ; i--){
 			for(int j = 0 ; j < i ; j++){
            
+				// Попарное сравнение элементов. Если не по возрастанию, меняем элементы местами.
+				
 				if( values[j] > values[j+1] ){
                 int change = values[j];
                 values[j] = values[j+1];
