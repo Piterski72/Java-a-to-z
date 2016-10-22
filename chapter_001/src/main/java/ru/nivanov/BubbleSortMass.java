aa5b6ee8557e7bc6d16f2bb1f30ec434fd9efd7a
Piterski72 <piterski@yandex.ru>
2016-10-22 10:55:42 +0300

    add Bubble massive sorting

---
+++ chapter_001/src/main/java/ru/nivanov/BubbleSortMass.java
@@ -0,0 +1,27 @@
+package ru.nivanov;
+
+public class BubbleSortMass {
+	int [] values;
+	
+	public BubbleSortMass(int[] values) {
+		this.values = values;
+	} 
+	
+	
+	public void bubbleSort(int[] values){
+    
+		for(int i = values.length-1 ; i > 0 ; i--){
+			for(int j = 0 ; j < i ; j++){
+           
+				if( values[j] > values[j+1] ){
+                int change = values[j];
+                values[j] = values[j+1];
+                values[j+1] = change;
+				}
+			}
+		}
+	}
+	
+	
+		
+}
