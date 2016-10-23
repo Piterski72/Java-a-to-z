00e1b3dc0a3e60cb9da773a7d085766b2bca0b69
Piterski72 <piterski@yandex.ru>
2016-10-23 19:07:24 +0300

    fix errs

--- chapter_001/src/test/java/ru/nivanov/BubbleSortMassTest.java
+++ chapter_001/src/test/java/ru/nivanov/BubbleSortMassTest.java
@@ -9,24 +9,10 @@ public class BubbleSortMassTest {
 	
 	@Test
 	public void whenSortCorrectThenReturnOk() {
-	int[] values=new int [5];
-	values [0] = 7;
-	values [1] = 12;
-	values [2] = 3;
-	values [3] = 8;
-	values [4] = 2;
+	int[] values={7,12,3,8,2};
 	BubbleSortMass testMas = new BubbleSortMass (values);
 	testMas.bubbleSort(values);
-	int [] expect=new int [5];
-	expect [0] = 2;
-	expect [1] = 3;
-	expect [2] = 7;
-	expect [3] = 8;
-	expect [4] = 12;
-	
-	
-    assertThat(values, is (expect)); 	 	
+	int [] expect={2,3,7,8,12};
+	assertThat(values, is (expect)); 	 	
 	}
-
-
 }
