39f1ece32d87761498e48c6aeb85c34a00a79495
Piterski72 <piterski@yandex.ru>
2016-10-25 13:01:40 +0300

    add massive rotation test

---
+++ chapter_001/src/test/java/ru/nivanov/Rotate90MassTest.java
@@ -0,0 +1,24 @@
+package ru.nivanov;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class Rotate90MassTest {
+	@Test
+	public void whenRotateCorrectThenReturnExpected() {
+		int[][] values = {{1,1,1,1},{2,2,2,2},{3,3,3,3},{4,4,4,4}};
+		Rotate90Mass testrot = new Rotate90Mass(values);
+		testrot.rotation90 (values);
+		int[][] expected = {{4,3,2,1},{4,3,2,1},{4,3,2,1},{4,3,2,1}};
+		assertThat(values, is (expected)); 	 	
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
