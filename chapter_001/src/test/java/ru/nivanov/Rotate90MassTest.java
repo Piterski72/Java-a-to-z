6e855c3a6327e403218e5d1715c302e0f2797e51
Piterski72 <piterski@yandex.ru>
2016-10-26 12:01:01 +0300

    corr Rotate test

--- chapter_001/src/test/java/ru/nivanov/Rotate90MassTest.java
+++ chapter_001/src/test/java/ru/nivanov/Rotate90MassTest.java
@@ -9,7 +9,7 @@ public class Rotate90MassTest {
 	public void whenRotateCorrectThenReturnExpected() {
 		int[][] values = {{1,1,1,1},{2,2,2,2},{3,3,3,3},{4,4,4,4}};
 		Rotate90Mass testrot = new Rotate90Mass(values);
-		testrot.rotation90 (values);
+		testrot.rotation90 ();
 		int[][] expected = {{4,3,2,1},{4,3,2,1},{4,3,2,1},{4,3,2,1}};
 		assertThat(values, is (expected)); 	 	
 		
