cd7e18b86f51fac1a5d2fb061eb87b514523b272
Piterski72 <piterski@yandex.ru>
2016-11-01 11:22:04 +0300

    corr3 Duplic

--- chapter_001/src/main/java/ru/nivanov/DuplicMass.java
+++ chapter_001/src/main/java/ru/nivanov/DuplicMass.java
@@ -16,23 +16,23 @@ public class DuplicMass {
 		
 		// finding copies and converting to nulls
 		for (int i = 0; i < max - 1; i++) {
-			if (symbols[i].equals(null)){
+			if (symbols[i].equals("")){
 				count ++;
 			}
 			for (int j  = i + 1; j < max; j++) {
 				
 				if (symbols[i].equals(symbols[j])) {
 					
-					symbols [j] = null;
+					symbols [j] = "";
 				}
 			}
 		}
 		
 		//moving nulls to the end of massive 
-		String tmp = null;
+		String tmp = "";
 		for (int i = max-1; i > 0; i--) {
 			for (int j = 0; j < i; j++){
-				if ( !symbols[max-j-1].equals(null) && symbols[max-j-2].equals(null)) {
+				if ( !symbols[max-j-1].equals("") && symbols[max-j-2].equals("")) {
 					tmp = symbols[max-j-2];
 					symbols[max-j-2] = symbols [max-j-1];
 					symbols[max-j-1] = tmp;
