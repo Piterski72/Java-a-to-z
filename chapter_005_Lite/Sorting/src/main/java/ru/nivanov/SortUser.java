f06486ed8fd74fac7227801a9d45c013508ee23c
Piterski72 <piterski@yandex.ru>
2017-04-17 16:11:43 +0300

    Collections lite, sorting task 2 added.

--- chapter_005_Lite/Sorting/src/main/java/ru/nivanov/SortUser.java
+++ chapter_005_Lite/Sorting/src/main/java/ru/nivanov/SortUser.java
@@ -1,8 +1,6 @@
 package ru.nivanov;
 
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * Created by Nikolay Ivanov on 17.04.2017.
@@ -17,6 +15,80 @@ class SortUser {
         TreeSet<User> sortedUsersByAge = new TreeSet<>();
         sortedUsersByAge.addAll(users);
         return sortedUsersByAge;
+    }
+
+    /**
+     * Sorting by hash code.
+     * @param users ..
+     * @return sorted list.
+     */
+    public List<User> sortHash(List<User> users) {
+        List<User> sortedByHashCode = new LinkedList<>();
+        sortedByHashCode.addAll(users);
+        sortedByHashCode.sort(new HashCompare());
+        return sortedByHashCode;
+    }
+
+    /**
+     * Sorting by name length.
+     * @param users ..
+     * @return sorted list.
+     */
+    public List<User> sortLength(List<User> users) {
+        List<User> sortedByNameLength = new LinkedList<>();
+        sortedByNameLength.addAll(users);
+        sortedByNameLength.sort(new NameLengthCompare());
+        return sortedByNameLength;
+    }
+
+    /**
+     * Hash compare inner class.
+     */
+    class HashCompare implements Comparator<User> {
+        /**
+         * Compares its two arguments for order.  Returns a negative integer,
+         * zero, or a positive integer as the first argument is less than, equal
+         * to, or greater than the second.
+         * @param userOne the first object to be compared.
+         * @param userTwo the second object to be compared.
+         * @return a negative integer, zero, or a positive integer as the
+         * first argument is less than, equal to, or greater than the
+         * second.
+         * @throws NullPointerException if an argument is null and this
+         * comparator does not permit null arguments
+         * @throws ClassCastException if the arguments' types prevent them from
+         * being compared by this comparator.
+         */
+        @Override
+        public int compare(User userOne, User userTwo) {
+            return Integer.valueOf(userOne.hashCode()).compareTo(userTwo.hashCode());
+        }
 
     }
+
+    /**
+     * Name length compare inner class.
+     */
+    class NameLengthCompare implements Comparator<User> {
+        /**
+         * Compares its two arguments for order.  Returns a negative integer,
+         * zero, or a positive integer as the first argument is less than, equal
+         * to, or greater than the second.<p>
+         * @param userOne the first object to be compared.
+         * @param userTwo the second object to be compared.
+         * @return a negative integer, zero, or a positive integer as the
+         * first argument is less than, equal to, or greater than the
+         * second.
+         * @throws NullPointerException if an argument is null and this
+         * comparator does not permit null arguments
+         * @throws ClassCastException if the arguments' types prevent them from
+         * being compared by this comparator.
+         */
+        @Override
+        public int compare(User userOne, User userTwo) {
+            return Integer.valueOf(userOne.getName().length()).compareTo(userTwo.getName().length());
+        }
+    }
+
+
 }
--- chapter_005_Lite/Sorting/src/test/java/ru/nivanov/SortUserTest.java
+++ chapter_005_Lite/Sorting/src/test/java/ru/nivanov/SortUserTest.java
@@ -4,29 +4,31 @@ import org.junit.Test;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Nikolay Ivanov on 17.04.2017.
  */
 public class SortUserTest {
+    private final int tri = 3;
+    private final User one = new User("Ann", tri);
+    private final User two = new User("Mike", 2);
+    private final User three = new User("Joshua", 1);
 
     /**
      * Test for sorting by age.
      */
     @Test
     public void whenSortByAgeThenReturnResult() {
-        final int tri = 3;
-        User one = new User("Ann", tri);
-        User two = new User("Mike", 2);
-        User three = new User("John", 1);
         LinkedList<User> usersList = new LinkedList<>();
-        usersList.add(one);
-        usersList.add(three);
-        usersList.add(two);
+        usersList.add(this.one);
+        usersList.add(this.three);
+        usersList.add(this.two);
         Set<User> result = new SortUser().sort(usersList);
         int[] expected = {1, 2, tri};
         Iterator<User> iterator = result.iterator();
@@ -37,6 +39,48 @@ public class SortUserTest {
             count++;
         }
         assertThat(expected, is(resultAges));
+    }
+
+    /**
+     * Test for sorting by hash.
+     */
+    @Test
+    public void whenSortedByHachThenReturnResult() {
+        LinkedList<User> usersByHash = new LinkedList<>();
+        usersByHash.add(this.one);
+        usersByHash.add(this.three);
+        usersByHash.add(this.two);
+        List<User> result = new SortUser().sortHash(usersByHash);
+        Iterator<User> iterator = result.iterator();
+        int[] resultHashes = new int[result.size()];
+        int count = 0;
+        while (iterator.hasNext()) {
+            resultHashes[count] = iterator.next().hashCode();
+            count++;
+        }
+        assertTrue(resultHashes[0] < resultHashes[1] && resultHashes[1] < resultHashes[2]);
+    }
+
+    /**
+     * Test for sorting by name length.
+     */
+    @Test
+    public void whenSortedByNameLengthThenReturnResult() {
+        LinkedList<User> usersByNameLength = new LinkedList<>();
+        usersByNameLength.add(this.three);
+        usersByNameLength.add(this.two);
+        usersByNameLength.add(this.one);
+        List<User> result = new SortUser().sortLength(usersByNameLength);
+        Iterator<User> iterator = result.iterator();
+        int[] resultNamesLength = new int[result.size()];
+        int count = 0;
+        while (iterator.hasNext()) {
+            resultNamesLength[count] = iterator.next().getName().length();
+            count++;
+        }
+        assertTrue(resultNamesLength[0] < resultNamesLength[1] && resultNamesLength[1] < resultNamesLength[2]);
+
+
     }
 
 }
