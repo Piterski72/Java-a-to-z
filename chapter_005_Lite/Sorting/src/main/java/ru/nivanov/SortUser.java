194b5906e01727b78b187558d706fb02b5ba2d14
Piterski72 <piterski@yandex.ru>
2017-04-18 13:00:18 +0300

    Collections lite, sorting task 2 corrections.

--- chapter_005_Lite/Sorting/src/main/java/ru/nivanov/SortUser.java
+++ chapter_005_Lite/Sorting/src/main/java/ru/nivanov/SortUser.java
@@ -1,6 +1,9 @@
 package ru.nivanov;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Created by Nikolay Ivanov on 17.04.2017.
@@ -11,7 +14,7 @@ class SortUser {
      * @param users ..
      * @return sorted list by users age.
      */
-    public Set<User> sort(List<User> users) {
+    Set<User> sort(List<User> users) {
         TreeSet<User> sortedUsersByAge = new TreeSet<>();
         sortedUsersByAge.addAll(users);
         return sortedUsersByAge;
@@ -22,11 +25,10 @@ class SortUser {
      * @param users ..
      * @return sorted list.
      */
-    public List<User> sortHash(List<User> users) {
-        List<User> sortedByHashCode = new LinkedList<>();
-        sortedByHashCode.addAll(users);
-        sortedByHashCode.sort(new HashCompare());
-        return sortedByHashCode;
+    List<User> sortHash(List<User> users) {
+        users.sort((userOne, userTwo) -> Integer.valueOf(userOne.hashCode()).compareTo(userTwo.hashCode()));
+
+        return users;
     }
 
     /**
@@ -34,61 +36,14 @@ class SortUser {
      * @param users ..
      * @return sorted list.
      */
-    public List<User> sortLength(List<User> users) {
-        List<User> sortedByNameLength = new LinkedList<>();
-        sortedByNameLength.addAll(users);
-        sortedByNameLength.sort(new NameLengthCompare());
-        return sortedByNameLength;
+    List<User> sortLength(List<User> users) {
+        users.sort(new Comparator<User>() {
+            @Override
+            public int compare(User userOne, User userTwo) {
+                return Integer.valueOf(userOne.getName().length()).compareTo(userTwo.getName().length());
+            }
+        });
+        return users;
     }
-
-    /**
-     * Hash compare inner class.
-     */
-    class HashCompare implements Comparator<User> {
-        /**
-         * Compares its two arguments for order.  Returns a negative integer,
-         * zero, or a positive integer as the first argument is less than, equal
-         * to, or greater than the second.
-         * @param userOne the first object to be compared.
-         * @param userTwo the second object to be compared.
-         * @return a negative integer, zero, or a positive integer as the
-         * first argument is less than, equal to, or greater than the
-         * second.
-         * @throws NullPointerException if an argument is null and this
-         * comparator does not permit null arguments
-         * @throws ClassCastException if the arguments' types prevent them from
-         * being compared by this comparator.
-         */
-        @Override
-        public int compare(User userOne, User userTwo) {
-            return Integer.valueOf(userOne.hashCode()).compareTo(userTwo.hashCode());
-        }
-
-    }
-
-    /**
-     * Name length compare inner class.
-     */
-    class NameLengthCompare implements Comparator<User> {
-        /**
-         * Compares its two arguments for order.  Returns a negative integer,
-         * zero, or a positive integer as the first argument is less than, equal
-         * to, or greater than the second.<p>
-         * @param userOne the first object to be compared.
-         * @param userTwo the second object to be compared.
-         * @return a negative integer, zero, or a positive integer as the
-         * first argument is less than, equal to, or greater than the
-         * second.
-         * @throws NullPointerException if an argument is null and this
-         * comparator does not permit null arguments
-         * @throws ClassCastException if the arguments' types prevent them from
-         * being compared by this comparator.
-         */
-        @Override
-        public int compare(User userOne, User userTwo) {
-            return Integer.valueOf(userOne.getName().length()).compareTo(userTwo.getName().length());
-        }
-    }
-
 
 }
