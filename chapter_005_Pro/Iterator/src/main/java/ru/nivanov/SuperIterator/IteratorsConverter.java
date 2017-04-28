13849538ebb1c80bdf157160a2b7bf50f9ef693e
Piterski72 <piterski@yandex.ru>
2017-04-28 19:11:41 +0300

    Collections pro, iterator 5.1.4. corrections

--- chapter_005_Pro/Iterator/src/main/java/ru/nivanov/SuperIterator/IteratorsConverter.java
+++ chapter_005_Pro/Iterator/src/main/java/ru/nivanov/SuperIterator/IteratorsConverter.java
@@ -1,7 +1,6 @@
 package ru.nivanov.SuperIterator;
 
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 
 /**
  * Created by Nikolay Ivanov on 26.04.2017.
@@ -36,19 +35,14 @@ public class IteratorsConverter implements ConverterOfIterators {
                     result = index.next();
                     return result;
 
-                }
-                if (index.hasNext()) {
+                } else if (index.hasNext()) {
                     result = index.next();
                     return result;
-                }
-                try {
+                } else {
                     index = it.next();
                     result = index.next();
                     return result;
-                } catch (NoSuchElementException nsee) {
-                    nsee.printStackTrace();
                 }
-                throw new NoSuchElementException();
             }
         };
     }
