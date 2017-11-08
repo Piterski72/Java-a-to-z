e9eb2117767613addaee181150b13efdb4e77e3a
Piterski72 <piterski@yandex.ru>
2017-11-08 05:37:42 +0300

    part 009 task 1. Структура клиент-серверных приложений added

---
+++ chapter_009/Task01/src/main/java/ru/nivanov/servlets/EchoServlet.java
@@ -0,0 +1,27 @@
+package ru.nivanov.servlets;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Nikolay Ivanov on 17.10.2017.
+ */
+public class EchoServlet extends HttpServlet {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.append("hello world");
+        writer.flush();
+    }
+}
